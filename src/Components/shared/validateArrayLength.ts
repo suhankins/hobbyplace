29ba68ffafe07f948b8cde19a7dc900fd958fd46
suhankins <punishedbernadetta@niuitmo.ru>
2023-02-12 23:22:29 +0400

    remade validateArrayLength

--- src/Components/shared/validateArrayLength.ts
+++ src/Components/shared/validateArrayLength.ts
@@ -1,8 +1,15 @@
-export function validateArrayLength(min: number, max: number) {
+/**
+ * Returns a validator, that checks if array is the correct length
+ * @param min minimum
+ * @param max maximum. If not provided, array must equal minimum
+ */
+export function validateArrayLength(min: number, max?: number) {
     return {
-        validator: (value: []) => value.length >= min && value.length <= max,
+        validator: (value: []) =>
+            value.length >= min &&
+            (max !== undefined ? value.length <= max : true),
         message:
-            min == max
+            max === undefined
                 ? `Path {PATH} must be the length of ${min}`
                 : `Path {PATH} must be longer than ${min} and shorter than ${max}`,
     };
