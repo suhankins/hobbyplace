030bdd40c308a408cb69bc31cd3d4b5437daeec3
suhankins <punishedbernadetta@niuitmo.ru>
2023-02-19 18:22:36 +0400

    navbar and drawer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,11 +10,15 @@ export default function RootLayout({
         <html lang="en">
             <head />
             <body>
-                <div className="navbar bg-base-100">
-                    <div className="flex-none lg:hidden">
-                        <button className="btn btn-square btn-ghost">
-                            <Burger />
-                        </button>
+                <div className="navbar bg-base-300">
+                    <div>
+                        <div className="drawer-content flex flex-col items-center justify-center">
+                            <label
+                                htmlFor="left-side-drawer"
+                                className="btn drawer-button lg:hidden">
+                                <Burger />
+                            </label>
+                        </div>
                     </div>
                     <div className="flex-1">
                         <a className="btn btn-ghost normal-case text-xl">
@@ -22,7 +26,29 @@ export default function RootLayout({
                         </a>
                     </div>
                 </div>
-                {children}
+                <div className="drawer drawer-mobile">
+                    <input
+                        id="left-side-drawer"
+                        type="checkbox"
+                        className="drawer-toggle"
+                    />
+                    <div className="drawer-content flex flex-col">
+                        {children}
+                    </div>
+                    <div className="drawer-side">
+                        <label
+                            htmlFor="left-side-drawer"
+                            className="drawer-overlay"></label>
+                        <ul className="menu p-4 w-80 bg-base-200 text-base-content">
+                            <li>
+                                <a>Login</a>
+                            </li>
+                            <li>
+                                <a>Register</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </body>
         </html>
     );
