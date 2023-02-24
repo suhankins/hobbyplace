793a3b8a9cd250b0b25094403dc6cd2097d58fc4
suhankins <punishedbernadetta@niuitmo.ru>
2023-02-24 10:35:56 +0400

    updated to next.js 13.2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,10 @@
 import './globals.css';
 
+export const  metadata = {
+    title: "Hobbyplace",
+    description: "TODO: Description"
+}
+
 export default function RootLayout({
     children,
 }: {
