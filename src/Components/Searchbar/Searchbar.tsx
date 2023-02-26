8860f021f4bcfb1fbdb51d7bdf2194684b2f6aed
suhankins <punishedbernadetta@niuitmo.ru>
2023-02-26 19:57:17 +0400

    disabled inout suggestions (temp)

--- src/Components/Searchbar/Searchbar.tsx
+++ src/Components/Searchbar/Searchbar.tsx
@@ -5,11 +5,12 @@ import { InputSuggestions } from 'react-input-suggestions';
 export function Searchbar(params?: { className?: string }) {
     // TODO: Make it actually work
     return (
-        <InputSuggestions
+        /*<InputSuggestions
             className={`input ${params ? params.className : ''}`}
             suggestions={['this', 'is', 'a', 'test'].map((word) => (
                 <span>{word}</span>
             ))}
-        />
+        />*/
+        <></>
     );
 }
