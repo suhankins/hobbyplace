863f3a08aa9dd7a2b40173b900132e3fdbef5f5b
suhankins <punishedbernadetta@niuitmo.ru>
2023-02-27 19:29:46 +0400

    language is now saved in a cookie

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,15 +4,16 @@ import type { NextRequest } from 'next/server';
 import { i18n } from './i18n-config';
 
 function getLocale(request: NextRequest) {
-    const prefered = request.headers.get('language');
+    const prefered = request.cookies.get('language');
     if (prefered === undefined || prefered === null) {
         return i18n.defaultLocale;
     }
-    return prefered;
+    return prefered.value;
 }
 
 export function middleware(request: NextRequest) {
     const pathname = request.nextUrl.pathname;
+
     const pathnameIsMissingLocale = i18n.locales.every(
         (locale) =>
             !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
@@ -25,6 +26,13 @@ export function middleware(request: NextRequest) {
             new URL(`/${locale}/${pathname}`, request.url)
         );
     }
+
+    const chosenLocale = pathname.split("/")[1];
+    
+    const response = NextResponse.next();
+    response.cookies.set("language", chosenLocale);
+
+    return response;
 }
 
 export const config = {
