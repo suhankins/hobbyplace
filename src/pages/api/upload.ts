35c2a41d0db82c1e4cfdd904289a5381c3b30f64
suhankins <punishedbernadetta@niuitmo.ru>
2023-03-01 22:27:26 +0400

    protected upload from unauthorized users

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -1,10 +1,20 @@
 import { SignedPostPolicyV4Output, Storage } from '@google-cloud/storage';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { getToken } from 'next-auth/jwt';
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from './auth/[...nextauth]';
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<SignedPostPolicyV4Output | string | void>
 ) {
+    const token = await getToken({ req });
+
+    if (!token) {
+        res.status(401).send();
+        return;
+    }
+
     const storage = new Storage({
         projectId: process.env.PROJECT_ID,
         credentials: {
