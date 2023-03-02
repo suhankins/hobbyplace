02cbc36f240aed94c6ff5fa1da73f1f236fa3f53
suhankins <punishedbernadetta@niuitmo.ru>
2023-03-02 17:13:13 +0400

    collection creation uses owner and requires auth

--- src/pages/api/collection/index.ts
+++ src/pages/api/collection/index.ts
@@ -5,11 +5,19 @@ import { parseJson } from '@/lib/parseJson';
 import { StatusCodes } from 'http-status-codes';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { CollectionField } from '@/Components/Fields/CollectionField';
+import { getToken } from 'next-auth/jwt';
+import { UserController } from '@/Components/User/UserController';
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<CollectionClass | string | void>
 ) {
+    const token = await getToken({ req });
+    if (!token) {
+        res.status(401).send();
+        return;
+    }
+
     const { method } = req;
 
     switch (method) {
@@ -22,11 +30,17 @@ export default async function handler(
             }
 
             try {
+                const user = await UserController.getByEmail(
+                    token.email as string
+                );
+                if (user === null) return;
+                
                 const result = await CollectionModel.create({
                     name: query.name as string,
                     description: query.description as string,
                     category: query.category as string,
                     fields: query.fields as CollectionField[],
+                    owner: user._id,
                 });
                 res.status(200).json(result);
             } catch (e) {
