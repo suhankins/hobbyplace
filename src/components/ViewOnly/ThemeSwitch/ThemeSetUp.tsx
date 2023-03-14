ac832959e39c6f5d9f97096dec3acf6a11728949
suhankins <punishedbernadetta@niuitmo.ru>
2023-03-14 22:47:06 +0400

    added comments to make code slightly more readable

--- src/components/ViewOnly/ThemeSwitch/ThemeSetUp.tsx
+++ src/components/ViewOnly/ThemeSwitch/ThemeSetUp.tsx
@@ -4,16 +4,25 @@ import { UpdateTheme } from './UpdateTheme';
 import { useEffect } from 'react';
 
 export function ThemeSetUp() {
-    useEffect(()=>{
+    useEffect(() => {
+        // Check if the code is being run in a browser
         if (typeof window !== 'undefined') {
+            // Check if the user's preferred theme is not set
             if (localStorage.theme === undefined) {
-                if (window.matchMedia('(prefers-color-scheme: dark)').matches)
+                // If the user's preferred theme is not set, check if their preferred color scheme is dark
+                if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+                    // If the user's preferred color scheme is dark, set the theme to dark
                     localStorage.theme = 'dark';
-                else localStorage.theme = 'light';
+                } else {
+                    // If the user's preferred color scheme is not dark, set the theme to light
+                    localStorage.theme = 'light';
+                }
             }
+
+            // Update the theme based on the user's preferences
             UpdateTheme();
         }
-    }, [])
+    }, []);
 
     return <></>;
 }
